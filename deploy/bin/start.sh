302795109de91f205148857092706c56352ce9bb
yanbit <yankunhadoop@gmail.com>
2016-01-21 15:18:09 +0800

    update hbase error

--- deploy/bin/start.sh
+++ deploy/bin/start.sh
@@ -3,5 +3,8 @@
 dir=`dirname "${BASH_SOURCE-$0}"`
 dir=`cd "$bin">/dev/null; pwd`
 echo "start rest server monitor log ..."
+cd $dir/../../
+mvn package -DskipTests
+cp target/monitor_log-0.1.jar deploy/lib/
 nohup java -cp $dir/../lib:$dir/../conf:$dir/../conf/*:$dir/../lib/*:$dir/../lib/monitor_log-0.1.jar com.elex.bigdata.web.RestMain >  /dev/null 2>&1 &
 echo "start rest server monitor log finish."
